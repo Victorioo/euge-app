90bbc93cb4abfc62dfe4d1b675a628632544ee2c
Victorio <victoriocaccioppoli@gmail.com>
2023-05-29 22:41:37 -0300

    V 1.0.2

---
+++ nombre/nombre.js
@@ -0,0 +1,15 @@
+function guardarValor() {
+    // obtener el valor
+
+    var nombre = document.getElementById("nombre").value;
+
+    localStorage.setItem("nombre", nombre);
+    
+    // console.log
+
+    console.log(`Guardado completo del nombre: ${nombre}`)
+
+    if (nombre != null && nombre != ""){
+        window.location.href = "land.html";
+    }
+}
